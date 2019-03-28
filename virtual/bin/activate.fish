3e957c79a9748f0260281619a7f31f5a991c4f61
roxaline <mariadelarosita078@gmail.com>
2019-03-28 14:01:14 +0200

    add static folder

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/roxaline/Desktop/instagram/virtual"
+set -gx VIRTUAL_ENV "/home/roxaline/Desktop/Instagram/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
